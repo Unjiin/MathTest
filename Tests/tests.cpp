5b149f03026b298fb9d535d9fcf3a5382814c619
Unjiin <pchelyshkin@mail.ru>
2026-09-17 00:42:10 +0300

    добавлена статистика и тесты MathTest

--- Tests/tests.cpp
+++ Tests/tests.cpp
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
-#include "../Lib/Task.h"
+#include "task.h"
+#include "mathtest.h"
 
 // Проверяем конструктор по умолчанию
 TEST(TaskTest, DefaultConstructor) {
@@ -58,4 +59,64 @@ TEST(TaskTest, MultiplyOperation) {
 
     EXPECT_EQ(task.operation, '*');
     EXPECT_EQ(task.answer, task.num_1 * task.num_2);
+}
+
+
+
+TEST(MathTestTest, ConstructorCount) {
+    MathTest test(5);
+
+    EXPECT_EQ(test.getCount(), 5);
+    EXPECT_EQ(test.getCorrectCount(), 0);
+}
+
+
+TEST(MathTestTest, RangeConstructor) {
+    MathTest test(10, 5, 20);
+
+    EXPECT_EQ(test.getCount(), 10);
+
+    for (int i = 0; i < test.getCount(); i++) {
+        Task task = test.getTask(i);
+
+        EXPECT_GE(task.num_1, 5);
+        EXPECT_LE(task.num_1, 20);
+
+        EXPECT_GE(task.num_2, 5);
+        EXPECT_LE(task.num_2, 20);
+    }
+}
+
+
+TEST(MathTestTest, OperationConstructor) {
+    MathTest test(10, 1, 20, '+');
+
+    for (int i = 0; i < test.getCount(); i++) {
+        Task task = test.getTask(i);
+
+        EXPECT_EQ(task.operation, '+');
+    }
+}
+
+
+TEST(MathTestTest, CorrectAnswer) {
+    MathTest test(5, 1, 10, '+');
+
+    Task task = test.getTask(0);
+
+    test.addAnswer(0, task.answer);
+
+    EXPECT_EQ(test.getUserAnswer(0), task.answer);
+    EXPECT_EQ(test.getCorrectCount(), 1);
+}
+
+
+TEST(MathTestTest, WrongAnswer) {
+    MathTest test(5, 1, 10, '+');
+
+    Task task = test.getTask(0);
+
+    test.addAnswer(0, task.answer + 1000);
+
+    EXPECT_EQ(test.getCorrectCount(), 0);
 }
